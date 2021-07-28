206346411d27288ed1349ec28a5a3f8625b9ce77
Kevin <kevinclegg@web.de>
2021-07-28 12:13:12 +0200

    Deklaration + Wertzuweisung

--- script_01.js
+++ script_01.js
@@ -1,2 +1,26 @@
 /***** Variablen 01 *******/
+/*Kommentar*/
+//Kommentar
+//Ausgabe
+
+// console.log("Hallo Welt !");
+// console.log(firstName); // nicht deklarierte variable ---> fehler
+
+
+// Deklaration + Wertzuweisung
+
+let firstname; // Deklaration ( Definition)
+firstName = "Max";  //Wetzuweiseung
+console.log(firstName); // Ausgabe
+
+let familyName="Mütze"; //Deklaration + Wertzuweisung
+console.log(familyName);// Ausgabe
+console.log(familyName + " " + firstName);//Ausgabe
+
+// 03 Deklaration + Wertzuweisung II
+
+
+
+
+
 
